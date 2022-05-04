91c3b40bf8fae08c6185ea1b09fcfef591876a26
faith62 <phifeichemu@gmail.com>
2022-05-04 10:31:26 +0300

    make API call

--- app/views.py
+++ app/views.py
@@ -1,22 +1,26 @@
 
 from flask import render_template
 from app import app
+from app.request import get_news
 
 # Views
 @app.route('/')
-def index():
+def new():
 
     '''
     View root page function that returns the index page and its data
     '''
     title = 'Home - Find the latest news'
-    return render_template('index.html', title = title)
 
-@app.route('/news/<id>')
-def movie(id):
+    bbc_news = get_news('bbc-news')
+    print(bbc_news)
+    return render_template('index.html', title = title, bbc = bbc_news)
 
-    '''
-    View movie page function that returns the movie details page and its data
-    '''
-    title = f'You are viewing {id}'
-    return render_template('news.html',title = title)
+# @app.route('/news/<id>')
+# def new(id):
+
+#     '''
+#     View movie page function that returns the movie details page and its data
+#     '''
+#     title = f'You are viewing {id}'
+#     return render_template('news.html',title = title)
