ed3bdc3e8a97e94063e4d392a9eb5c5551150e0f
faith62 <phifeichemu@gmail.com>
2022-05-04 09:02:30 +0300

    add dynamic rout

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from email import message
 from flask import render_template
 from app import app
 
@@ -8,4 +9,13 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    message='news today'
+    return render_template('index.html', message=message)
+
+@app.route('/news/<id>')
+def movie(movie_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('news.html',id = id)
