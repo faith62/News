3419104d657104dfa861f7820effc0740643d5a9
faith62 <phifeichemu@gmail.com>
2022-05-04 09:55:37 +0300

    create news class

---
+++ app/models/news.py
@@ -0,0 +1,14 @@
+class News:
+    '''
+    News class to define news Objects
+    '''
+
+    def __init__(self,id ,author, title, description,url,urlToimage,publishedAt,content):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToimage = urlToimage
+        self.publishedAt = publishedAt
+        self.content = content
